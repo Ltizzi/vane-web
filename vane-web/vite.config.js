122365492d69c95fc0aec74de8babfb8618bd192
Ltizzi <terlizzileonardo@gmail.com>
2026-02-12 14:33:31 -0300

    added better SEO optimizations

--- vane-web/vite.config.js
+++ vane-web/vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import vitePluginSitemap from "vite-plugin-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vitePluginSitemap({
-      hostname: "https://licvanesacarlottomiranda.com.ar",
-      readable: true,
-      routes: ["/", "/faq", "/about", "/contact", "/info"],
-    }),
   ],
   resolve: {
     alias: {
